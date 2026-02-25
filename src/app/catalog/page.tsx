76c3bfe4ef58b93161c46d684c6e19945ad33da2
Cocktail Design <dimikravets@yandex.ru>
2026-02-25 19:59:18 +0700

    Обновить страницу каталога: стили и логика /catalog

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -14,6 +14,28 @@ export const metadata = pageMetadata({
   canonical: "/catalog",
 });
 
+function formatProductsCount(count: number): string {
+  const lastTwo = count % 100;
+  const last = count % 10;
+
+  //ислючение
+  if (lastTwo >= 11 && lastTwo <= 14) {
+    return `${count} товаров`;
+  }
+
+  // 1 товар
+  if (last === 1) {
+    return `${count} товар`;
+  }
+
+  // 2-4 товара
+  if (last >= 2 && last <= 4) {
+    return `${count} товара`;
+  }
+
+  return `${count} товаров`;
+}
+
 export default async function CatalogPage() {
   // Загружаем категории из Strapi через наш API-слой
   const categories = await getTopCategoriesFromStrapi();
@@ -30,8 +52,20 @@ export default async function CatalogPage() {
             <li key={category.id} className={styles.card}>
               <Link href={`/catalog/c/${category.slug}`} className={styles.cardLink}>
                 {/* Название категории */}
-                <span className={styles.name}>{category.name}</span>
-                <span className={styles.name}>{category.productsCount}</span>
+                <div className={styles.cardText}>
+                  <h3 className={styles.title}>{category.name}</h3>
+                  <span className={styles.count}>{formatProductsCount(category.productsCount)}</span>
+                </div>
+
+                {/* Контейнер изображения */}
+                <div className={styles.imageWrapper}>
+                  <Image
+                    src={category.imageSrc || "/placeholder-image.jpg"}
+                    fill
+                    alt={category.name ? `Категория: ${category.name}` : "Категория"}
+                    className={styles.image}
+                  />
+                </div>
               </Link>
             </li>
           ))}
